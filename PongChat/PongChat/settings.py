94fc22c3d3831134d8ad17cff26cd583c6196f47
yuki-shimoda <yuki-shimoda@protonmail.com>
2024-04-10 10:40:51 +0000

    [fix]: #7 fix mypy style
    [body]
    
    [notes]
    
    close #7

--- PongChat/PongChat/settings.py
+++ PongChat/PongChat/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = "django-insecure-141^z_11w3^(b&r3v%7u7sw*pv#fqzh@@l4swzy)i3r2r4h%-q
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS: str = []
+ALLOWED_HOSTS: list[str] = []
 
 
 # Application definition
