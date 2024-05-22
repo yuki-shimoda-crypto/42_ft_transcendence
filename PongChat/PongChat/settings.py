dd8cd1d53ed2bcf725463e4b04b5b129927c467e
Yono1112 <yuma.ohno.sss@gmail.com>
2024-05-22 04:15:38 +0000

    [refactor]: format black
    [body]
    
    [notes]
    
    close #<issue-number>

--- PongChat/PongChat/settings.py
+++ PongChat/PongChat/settings.py
@@ -19,7 +19,7 @@ env = environ.Env()
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-environ.Env.read_env(os.path.join(BASE_DIR, '../.env'))
+environ.Env.read_env(os.path.join(BASE_DIR, "../.env"))
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
@@ -27,7 +27,7 @@ environ.Env.read_env(os.path.join(BASE_DIR, '../.env'))
 # SECURITY WARNING: keep the secret key used in production secret!
 # SECRET_KEY = "django-insecure-%hr6m+*yolj1%5eg4*2)!%2g0-@csu!j&k92@)$1lsyc3#qi^3"
 try:
-    SECRET_KEY = env('SECRET_KEY')
+    SECRET_KEY = env("SECRET_KEY")
 except environ.ImproperlyConfigured:
     raise Exception("SECRET_KEYが設定されていません。")
 
