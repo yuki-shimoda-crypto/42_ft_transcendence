043355dd0d861dae5aecf484d5fdc0b6fbe8a283
Yono1112 <yuma.ohno.sss@gmail.com>
2024-05-22 04:20:02 +0000

    [refactor]: format code by isort
    [body]
    
    [notes]
    
    close #<issue-number>

--- PongChat/PongChat/settings.py
+++ PongChat/PongChat/settings.py
@@ -10,9 +10,10 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.0/ref/settings/
 """
 
-from pathlib import Path
-import environ
 import os
+from pathlib import Path
+
+import environ
 
 env = environ.Env()
 
