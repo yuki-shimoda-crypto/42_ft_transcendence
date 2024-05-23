daca533ecc07dd52c670fd7abdb5d277f8d6f76b
yuki-shimoda <yuki-shimoda@protonmail.com>
2024-05-23 21:26:59 +0900

    [fix]: #53 fix linter
    [body]
    
    [notes]
    
    close #53

--- PongChat/PongChat/settings.py
+++ PongChat/PongChat/settings.py
@@ -82,11 +82,11 @@ WSGI_APPLICATION = "PongChat.wsgi.application"
 DATABASES = {
     "default": {
         "ENGINE": "django.db.backends.postgresql",
-        "NAME": os.getenv('DATABASE_NAME'),
-        "USER": os.getenv('DATABASE_USER'),
-        "PASSWORD": os.getenv('DATABASE_PASSWORD'),
-        "HOST": os.getenv('DATABASE_HOST'),
-        "PORT": os.getenv('DATABASE_PORT'),
+        "NAME": os.getenv("DATABASE_NAME"),
+        "USER": os.getenv("DATABASE_USER"),
+        "PASSWORD": os.getenv("DATABASE_PASSWORD"),
+        "HOST": os.getenv("DATABASE_HOST"),
+        "PORT": os.getenv("DATABASE_PORT"),
     }
 }
 
