fe8e0faad987bb2889359ab9d9af46e4b1063555
Yono1112 <yuma.ohno.sss@gmail.com>
2024-05-24 11:47:14 +0000

    [refactor]: #23 change LANGUAGE_CODE from en to ja
    [body]
    
    [notes]
    
    close #<issue-number>

--- PongChat/PongChat/settings.py
+++ PongChat/PongChat/settings.py
@@ -116,7 +116,7 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/5.0/topics/i18n/
 
-LANGUAGE_CODE = "en-us"
+LANGUAGE_CODE = "ja"
 
 TIME_ZONE = "Japan"
 
