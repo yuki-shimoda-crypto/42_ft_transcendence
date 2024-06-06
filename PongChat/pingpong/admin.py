6cb9be9104aae60f18ca80d4cc7e714ac3c2db95
yuki-shimoda <yuki-shimoda@protonmail.com>
2024-06-06 22:09:47 +0900

    [feat]: Game, Match Modelの作成
    新機能:
    - 管理画面にGameおよびMatchモデルを追加
    - Gameモデルの作成: プレイヤー、スコア、ステータス、開始日、終了日、勝者フィールドを含む
    - Matchモデルの作成: プレイヤー、ゲーム、プレイ日時を含む
    - リモートプレイヤー機能の有効化: 異なるコンピュータ間で同じゲームにアクセスし、プレイ可能
    
    このコミットは、ユーザーがリモートでPongゲームをプレイできる機能を追加します。
    
    close #45

--- PongChat/pingpong/admin.py
+++ PongChat/pingpong/admin.py
@@ -1,3 +1,8 @@
-# from django.contrib import admin
+from django.contrib import admin
+
+from .models import Game, Match
 
 # Register your models here.
+
+admin.site.register(Game)
+admin.site.register(Match)
