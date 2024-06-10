ae045b6d6425375259cc1732d04a94f8cdf1c9b5
yuki-shimoda <yuki-shimoda@protonmail.com>
2024-06-10 20:18:43 +0900

    feat: #45 サイドバーの改善とURL設定の追加
    [body]:
    - `pingpong/urls.py`に`app_name`を追加し、名前空間を設定
    - サイドバーのタイトルを「Pong Chat」に変更
    - サイドバーリンクに「Home」「Chat」「Game」「Login」を追加
    - サイドバーリンクのURLを設定し、ゲームページとログインページへのナビゲーションを可能に
    
    [note]:
    - サイドバーのデザインとリンクを改善し、より使いやすく調整
    
    close #<issue-number>

--- PongChat/pingpong/urls.py
+++ PongChat/pingpong/urls.py
@@ -2,6 +2,8 @@ from django.urls import path
 
 from . import views
 
+app_name = "pingpong"
+
 urlpatterns = [
     path("", views.index, name="index"),
     path("create_game", views.create_game, name="create_game"),
