7bf777580d71dade677ded2deeb9e1aa3c5b5b14
susui <96704644+sora-grayscale@users.noreply.github.com>
2024-06-17 08:49:28 +0900

    [feat]: #35 send-direct-message-to-other-users
    [body]
    - commentout public-chat view
    
    [notes]

--- PongChat/chat/urls.py
+++ PongChat/chat/urls.py
@@ -4,7 +4,7 @@ from .views import chat_view, get_or_create_chatroom, profile_view, user_list
 
 urlpatterns = [
     path("", user_list, name="chat-home"),
-    path("chat", chat_view, name="public-chat"),
+    # path("chat", chat_view, name="public-chat"),
     path("<username>", get_or_create_chatroom, name="start-chat"),
     path("room/<chatroom_name>", chat_view, name="chatroom"),
     path("@<username>/", profile_view, name="profile"),
