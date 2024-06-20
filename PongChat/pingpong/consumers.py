86a6307b059022a13209f7bca823538daf287a5c
yuki-shimoda <yuki-shimoda@protonmail.com>
2024-06-20 21:17:40 +0900

    [feat]: #45 WebSocketマッチング機能の実装
    新機能:
    - `GameConsumer`クラスを拡張し、マッチングロジックを追加
    - `multiplayer_lobby.html`テンプレートを更新し、マッチングボタンとステータスメッセージを追加
    - WebSocket URLパターンを修正して、ルーム名なしのパスを使用
    
    修正内容:
    - プレイヤーがマッチングボタンをクリックすると、WebSocket接続を確立し、マッチングリクエストを送信
    - マッチが見つかると、プレイヤーは新しいゲームセッションにリダイレクトされる
    
    close #45

--- PongChat/pingpong/consumers.py
+++ PongChat/pingpong/consumers.py
@@ -1,51 +1,54 @@
 import json
+from urllib.parse import urljoin
+import random
 from channels.generic.websocket import AsyncWebsocketConsumer
 
 
-# class GameConsumer(AsyncWebsocketConsumer):
-#     async def connect(self):
-#         await self.accept()
-
-#     async def disconnect(self, close_code):
-#         pass
-
-#     # Receive message from WebSocket
-#     async def receive(self, text_data):
-#         data = json.loads(text_data)
-#         action = data["action"]
-
-#         if action == "request_match":
-#             opponent_username = data["opponent"]
-#             # Send the match request to the specified opponent
-#             await self.channel_layer.group_send(
-#                 f"user_{opponent_username}",
-#                 {
-#                     "type": "match_request",
-#                     "from_user": self.scope["user"].username,
-#                 }
-#             )
-
-#     async def match_request(self, event):
-#         # Forward the match request to the user
-#         from_user = event["from_user"]
-#         await self.send(text_data=json.dumps({
-#             "action": "match_request",
-#             "from_user": from_user,
-#         }))
-
 class GameConsumer(AsyncWebsocketConsumer):
-
     async def connect(self):
-        print("Connected")
         await self.accept()
+        await self.channel_layer.group_add(
+            "waiting_room",
+            self.channel_name
+        )
 
     async def disconnect(self, close_code):
-        print("Disconnected")
+        await self.channel_layer.group_discard(
+            "waiting_room",
+            self.channel_name
+        )
 
     async def receive(self, text_data):
-        print("Received")
-        await self.send(text_data=f"Message: {text_data}")
+        data = json.loads(text_data)
+        action = data.get("action")
+        if action == "start_match":
+            players = await self.channel_layer.group_list("waiting_room")
+            if len(players) >= 2:
+                chosen_players = random.sample(players, 2)
+                await self.start_game_session(chosen_players)
 
-    async def send_message(self, res):
-        print("Sent message")
-        await self.send(text_data=res)
+    async def start_game_session(self, players):
+        game_id = f"game_{random.randint(1000, 9999)}"
+        host = self.scope["headers"].get(b"host", b"localhost").decode("utf-8")
+        scheme = self.scope["scheme"]
+        base_url = f"{scheme}://{host}"
+        game_url = urljoin(base_url, f"/game/{game_id}/")
+
+        for player in players:
+            await self.channel_layer.group_add(game_id, player)
+            await self.channel_layer.group_discard("waiting_room", player)
+            await self.channel_layer.send(
+                player,
+                {
+                    "type": "game.start",
+                    "game_id": game_id,
+                    "message": "match_found",
+                    "url": game_url
+                }
+            )
+
+    async def game_start(self, event):
+        await self.send(text_data=json.dumps({
+            "game_id": event["game_id"],
+            "message": event["message"]
+        }))
--- PongChat/pingpong/routing.py
+++ PongChat/pingpong/routing.py
@@ -2,5 +2,6 @@ from django.urls import re_path
 from .consumers import GameConsumer
 
 websocket_urlpatterns = [
-    re_path(r"ws/game/(?P<room_name>\w+)/$", GameConsumer.as_asgi())
+    # re_path(r"ws/game/(?P<room_name>\w+)/$", GameConsumer.as_asgi())
+    re_path(r"ws/game/$", GameConsumer.as_asgi())
 ]
