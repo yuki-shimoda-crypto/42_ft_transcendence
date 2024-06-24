afa1b145a8cc8db2ff426d1fa11683860e2c51c0
yuki-shimoda <yuki-shimoda@protonmail.com>
2024-06-24 19:24:43 +0900

    [style]: #45 lint python
    [body]
    
    [notes]
    
    close #<issue-number>

--- PongChat/accounts/migrations/0005_alter_customuser_groups_alter_customuser_is_active_and_more.py
+++ PongChat/accounts/migrations/0005_alter_customuser_groups_alter_customuser_is_active_and_more.py
@@ -17,7 +17,8 @@ class Migration(migrations.Migration):
             name="groups",
             field=models.ManyToManyField(
                 blank=True,
-                help_text="The groups this user belongs to. A user will get all permissions granted to each of their groups.",
+                help_text="The groups this user belongs to.\
+                    A user will get all permissions granted to each of their groups.",
                 related_name="user_set",
                 related_query_name="user",
                 to="auth.group",
@@ -29,7 +30,8 @@ class Migration(migrations.Migration):
             name="is_active",
             field=models.BooleanField(
                 default=True,
-                help_text="Designates whether this user should be treated as active. Unselect this instead of deleting accounts.",
+                help_text="Designates whether this user should be treated as active.\
+                    Unselect this instead of deleting accounts.",
                 verbose_name="active",
             ),
         ),
@@ -38,7 +40,8 @@ class Migration(migrations.Migration):
             name="is_superuser",
             field=models.BooleanField(
                 default=False,
-                help_text="Designates that this user has all permissions without explicitly assigning them.",
+                help_text="Designates that this user\
+                    has all permissions without explicitly assigning them.",
                 verbose_name="superuser status",
             ),
         ),
@@ -47,7 +50,8 @@ class Migration(migrations.Migration):
             name="username",
             field=models.CharField(
                 error_messages={"unique": "A user with that username already exists."},
-                help_text="Required. 150 characters or fewer. Letters, digits and @/./+/-/_ only.",
+                help_text="Required. 150 characters or fewer.\
+                    Letters, digits and @/./+/-/_ only.",
                 max_length=150,
                 unique=True,
                 validators=[django.contrib.auth.validators.UnicodeUsernameValidator()],
