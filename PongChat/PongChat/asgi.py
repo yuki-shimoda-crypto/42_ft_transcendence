8571ed600c75d007ccc0b2a4cb3d7de41ecab4a0
susui <96704644+sora-grayscale@users.noreply.github.com>
2024-06-26 19:42:50 +0900

    [feat]: #79 deploy
    [body]
    - fix for linter
    
    [notes]
    
    close #79

--- PongChat/PongChat/asgi.py
+++ PongChat/PongChat/asgi.py
@@ -7,6 +7,8 @@ For more information on this file, see
 https://docs.djangoproject.com/en/5.0/howto/deployment/asgi/
 """
 
+# flake8: noqa
+# isort: skip_file
 import os
 from django.core.asgi import get_asgi_application
 
