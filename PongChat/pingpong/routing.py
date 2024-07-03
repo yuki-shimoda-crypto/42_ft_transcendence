57947caa77cbdafe11b0f30a9c45171dad494f95
susui <96704644+sora-grayscale@users.noreply.github.com>
2024-07-03 16:48:14 +0900

    [feat]: #83 feat-top-page
    [body]
    - create chat/routing.py
    - fix asgi.py
    
    [notes]
    
    close #<issue-number>

--- PongChat/pingpong/routing.py
+++ PongChat/pingpong/routing.py
@@ -1,10 +1,8 @@
-from chat.consumers import ChatroomConsumer
-from django.urls import path, re_path
+from django.urls import re_path
 
 from .consumers import GameConsumer
 
 websocket_urlpatterns = [
     # re_path(r"ws/game/(?P<room_name>\w+)/$", GameConsumer.as_asgi())
     re_path(r"ws/game/$", GameConsumer.as_asgi()),
-    path("ws/chatroom/<chatroom_name>", ChatroomConsumer.as_asgi()),
 ]
